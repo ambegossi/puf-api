9e786907c92bb2976a34ceef54be84e8bd47bdf2
Anderson Begossi <andersonbegossi@gmail.com>
2021-09-27 22:28:52 -0300

    chore: configure babel and build process

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    ['module-resolver', { alias: { '~': './src', '@': '.' } }],
+  ],
+}
