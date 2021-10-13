36e8f2f2f36ab2b5f1fe8021e208149bcdaedb7c
Anderson Begossi <andersonbegossi@gmail.com>
2021-10-12 21:15:08 -0300

    feat: add body parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
 
 import { router } from './routes'
 
 const app = new Koa()
 
+app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
